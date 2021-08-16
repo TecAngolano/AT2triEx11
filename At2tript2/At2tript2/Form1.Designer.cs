e543369028f8c1428280dd6f31c6fd88838ca799
Vitor <vitorcesarino1@gmail.com>
2021-08-16 08:51:54 -0300

    AT2

---
+++ At2tript2/At2tript2/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace At2tript2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(12, 27);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(118, 23);
+            this.txtAltura.TabIndex = 0;
+            this.txtAltura.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Location = new System.Drawing.Point(12, 74);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(118, 23);
+            this.txtPeso.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Altura (em metros)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Peso (em toneladas)";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(12, 104);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(67, 23);
+            this.btn1.TabIndex = 4;
+            this.btn1.Text = "Calcular";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(144, 161);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.txtAltura);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ At2tript2/At2tript2/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace At2tript2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int altura, peso;
+            altura = Convert.ToInt32(txtAltura.Text);
+            peso = Convert.ToInt32(txtPeso.Text);
+            if (peso <= 6 && altura <= 10)
+            {
+                MessageBox.Show("Pode passar encima da ponte e enbaixo");
+            }
+            else if (peso <= 6 && altura > 10)
+            {
+                MessageBox.Show("Não pode passar enbaixo da ponte mas pode passar encima");
+            }
+            else if (peso > 6 && altura <= 10)
+            {
+                MessageBox.Show("Pode passar enbaixo da ponte mas não encima");
+            }
+            else if (peso > 6 && altura > 10)
+            {
+                MessageBox.Show("Não pode passar enbaixo da ponte nem encima");
+            }
+            else if (peso == 6 && altura == 10)
+            {
+                MessageBox.Show("Pode passar encima da ponte e enbaixo");
+            }  
+        }
+    }
+}
